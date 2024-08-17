41d030d55e73bdf8ff58be6f6be3d092c9f34e81
Sawan Vitor Pereira <sawanpereira@gmail.com>
2024-08-17 12:07:06 -0300

    upload2

--- form_principal.pas
+++ form_principal.pas
@@ -4,10 +4,13 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls;
 
 type
   TForm25 = class(TForm)
+    btn_sawan: TButton;
+    procedure btn_sawanClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +23,10 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TForm25.btn_sawanClick(Sender: TObject);
+begin
+  ShowMessage('sawan');
+end;
 
 end.
